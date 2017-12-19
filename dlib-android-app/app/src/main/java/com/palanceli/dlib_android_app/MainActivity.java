a39e3361e3905bd7197f4896b16953aa510e826b
palanceli <palanceli@163.com>
2017-12-19 22:21:21 +0800

    commit changes

--- dlib-android-app/app/src/main/java/com/palanceli/dlib_android_app/MainActivity.java
+++ dlib-android-app/app/src/main/java/com/palanceli/dlib_android_app/MainActivity.java
@@ -1,13 +1,23 @@
 package com.palanceli.dlib_android_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Click;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Click({R.id.fab_cam})
+    protected void launchCameraPreview(){
+        startActivity(new Intent(this, CameraActivity.class));
     }
 }
